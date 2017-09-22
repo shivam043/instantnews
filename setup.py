47dace604ef19acff634d6c72456146636dc8e14
shivam singh <shivam043@gmail.com>
2017-09-22 11:17:55 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+      name='instantnews',
+      version='1.0',
+      description='Get live news instantaneously',
+      author='shivam singh',
+      author_email='shivam043@gmail.com',
+      url='https://github.com/shivam043/instantnews',
+      license='MIT',
+      py_modules=['instantnews'],
+      install_requires=[
+      'requests','colored'
+      ],
+      entry_points='''
+      [console_scripts]
+      instantnews=instantnews:parser
+      ''',
+      
+)
+      
