f62fc2dbef2d510f81f4c3e88fa421039bf03428
shivam singh <shivam043@gmail.com>
2017-09-22 18:40:04 +0530

    updated to version 1.0.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
       name='instantnews',
-      version='1.0',
+      version='1.0.7',
       description='Get live news instantaneously',
       author='shivam singh',
       author_email='shivam043@gmail.com',
