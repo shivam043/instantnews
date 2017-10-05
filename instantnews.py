be457bdde7a08056631c7844dd32b6de8859499a
Connor Kendrick <connorkendrickmusic@gmail.com>
2017-10-05 16:35:05 -0400

    Refactor code (#11)
    * Restructure code
    
    * Improve variable names
    
    * Simplify code
    
    * Rewrite console output to be more intuitive
    
    * Add argument and function to display all categories

--- instantnews.py
+++ instantnews.py
@@ -1,183 +1,184 @@
 #!/usr/bin/env python
-'''
+"""
 Author:Shivam Singh
 mail:shivam043@gmail.com
 copyright@2017
-'''
+"""
 
-import requests
+
+import os
+import sys
 import argparse
 import webbrowser
-import sys
-import os
+import requests
 
-API_URL="https://newsapi.org/register"
-BASE_URL="https://newsapi.org/v1/articles"
-SOURCE_URL="https://newsapi.org/v1/sources"
-valid=['y','n']
-news_code=['1','2']
-category_news=['business','entertainment','gaming','general','music','politics','science-and-nature','sport','technology']
 
-''' Function to test network connection '''
+# Global constants
+API_URL = "https://newsapi.org/register"
+BASE_URL = "https://newsapi.org/v1/articles"
+SOURCE_URL = "https://newsapi.org/v1/sources"
+VALID = ['y', 'n']
+NEWS_CATEGORIES = [
+    'business', 'entertainment', 'gaming',
+    'general', 'music', 'politics',
+    'science-and-nature', 'sport', 'technology'
+    ]
+
+
+# Global variables
+news_codes = []
+api_key = ""
+
+
 def test_network_connection():
-   try:
-       r=requests.get("https://newsapi.org/")
-       r.raise_for_status()
-   except requests.exceptions.ConnectionError:
-       print("There was issue connecting to the server. Please check your network connection.")
-       sys.exit(1)
-   except requests.exceptions.RequestException as e:
-       print(e)
-       sys.exit(1)
+    """Test network connection"""
+    try:
+        response = requests.get("https://newsapi.org/")
+        response.raise_for_status()
+    except requests.exceptions.ConnectionError:
+        print("There was issue connecting to the server. Please check your network connection.")
+        sys.exit(1)
+    except requests.exceptions.RequestException as exception:
+        print(exception)
+        sys.exit(1)
 
 
+def fetch_all_news_codes():
+    """Fetch news codes from all sources"""
+    response = requests.get(SOURCE_URL)
+    json = response.json()
+    global news_codes
+    for source in json['sources']:
+        news_codes.append(source['id'])
 
-''' Fetching all the news code  '''
-def fetch_all_news_code():
-    r=requests.get(SOURCE_URL)
-    t=r.json()
-    for i in t['sources']:
-        news_code.append(i['id'])
-    
-
-''' Load api key on a global apikey and validate it  '''
 
 def load_config_key():
-    
+    """Load api key on a global api key and validate it"""
     try:
-        global apiKey
-        apiKey=os.environ['IN_API_KEY']
-        if len(apiKey)==32:
+        global api_key
+        api_key = os.environ['IN_API_KEY']
+        if len(api_key) == 32:
             try:
-                int(apiKey,16)
+                int(api_key, 16)
             except ValueError:
                 print("Invalid API key")
-    
     except KeyError:
         print('No API Token detected. '
               'Please visit {0} and get an API Token, '
               'which will be used by instantnews '
               'to get access to the data.'
-             .format(API_URL))
+              .format(API_URL))
         sys.exit(1)
 
-''' Function to validate choice for yes or no'''
 
 def check_choice(choice):
-    
-    if len(choice)>1:
-        return True
-    
-    if choice=="y" or choice=="n":
-        return False
-'''Function for displaying news code category wise'''
+    """Validate choice for yes or no"""
+    return choice == 'y' or choice == 'n'
 
-def show_sources_category(l):
-   
-    flag=0
-    if l in category_news:
-        flag=1
-    if flag==0:
-        print("Enter valid category")
+
+def show_sources_category(category):
+    """Display news codes by category"""
+    if category not in NEWS_CATEGORIES:
+        print("Invalid category")
         sys.exit(1)
 
-    url="?category={category_type}"
-    r=requests.get((SOURCE_URL+url).format(category_type=l))
-    t=r.json()
-    for i in t['sources']:
-        print(u"{0}: <{1}> {2}".format("News Code",i['id'],i['name']))
+    url = "?category={category_type}"
+    response = requests.get((SOURCE_URL+url).format(category_type=category))
+    json = response.json()
+    for source in json['sources']:
+        print(u"{0}: <{1}> {2}".format("News Code", source['id'], source['name']))
 
-'''Function for displaying all news code'''
 
 def show_sources_all():
-    r=requests.get(SOURCE_URL)
-    t=r.json()
-    for i in t['sources']:
-        print(u"{0}: <{1}> {2}".format("News Code",i['id'],i['name']))
+    """Display all news codes"""
+    response = requests.get(SOURCE_URL)
+    json = response.json()
+    for source in json['sources']:
+        print(u"{0}: <{1}> {2}".format("News Code", source['id'], source['name']))
 
-'''Function for displaying news w.r.t news id'''
 
-def show_news(l,BASE_URL):
-    
-    url="?source={news_id}&apiKey="
-    
-    r=requests.get((BASE_URL+url+apiKey).format(news_id=l))
-    t=r.json()
-    list_news=[]
-    c=0
-    
-    for i in t['articles']:
-        print('[{0}] {1}: {2}'.format(c,"Title",i['title']))
-        print('{0}: {1}'.format("Author",i['author']))
-        print('{0}: {1}'.format("Summary",i['description']))
+def show_categories():
+    """Display all news categories"""
+    for category in NEWS_CATEGORIES:
+        print(category)
+
+
+def show_news(code, BASE_URL):
+    """Display news with respect to news id"""
+    url = "?source={news_id}&apiKey="
+    response = requests.get((BASE_URL+url+api_key).format(news_id=code))
+    json = response.json()
+
+    article_list = []
+    for article in json['articles']:
+        print('[{0}] {1}: {2}'.format(len(article_list), "Title", article['title']))
+        print('{0}: {1}'.format("Author", article['author']))
+        print('{0}: {1}'.format("Summary", article['description']))
         print('--------------------------------------------')
-        list_news.append(i['url'])
-        c=c+1
-    
-    print("Want to see the news that interests you/open in a webpage? Enter Y/N ")    
-    
-    choice=(input()).lower()
-    while check_choice(choice):
-          print("Ooops that was wrong,Try again!")
-          choice=input("Enter (y/n): ").lower()
+        article_list.append(article['url'])
 
-    
-    if choice=='y':
-        
-        while choice=='y':
-            
-            news_code=input("Enter news code: ")
-            while not(news_code.isdigit()) or not (0<=int(news_code)<=c):
-                print("Ooops that was wrong,Try again!")
-                news_code=input("Pick One: ")
-            
-            webbrowser.open(list_news[int(news_code)])
-            print("Want to exit? Enter Y/N ")
-            choice=(input()).lower()
-            if choice not in valid:
-               sys.exit()
+    print("Would you like to open an article in a web browser? Enter (y/n): ")
 
-            if choice=='n':
-                choice='y'
+    choice = (input()).lower()
+    if choice not in VALID:
+        print("Invalid entry.")
+        sys.exit()
+
+    if choice == 'y':
+        while choice == 'y':
+            article_number = input("Enter an article number: ")
+            while not article_number.isdigit() or not 0 <= int(article_number) <= len(article_list):
+                print("Invalid article number. Try again.")
+                article_number = input("Enter an article number: ")
+
+            webbrowser.open(article_list[int(article_number)])
+
+            print("Would you like to view another article? Enter (y/n): ")
+            choice = (input()).lower()
+            if choice not in VALID:
+                sys.exit()
+            if choice == 'y':
                 continue
-            else:
-                break
-'''Arguments building fun d: '''
+
 
 def parser():
-    
-    fetch_all_news_code()
+    """Parse arguments and call appropriate functions"""
+    fetch_all_news_codes()
     load_config_key()
+
     if not sys.argv[1:]:
-        print("Arguments need Type in --help/-h for help")
+        print("Arguments needed. Use argument --help/-h for more information.")
     else:
-        
-        parser=argparse.ArgumentParser()
-        parser.add_argument("--show","-s",action="store",help="Shows all the news channel codes category wise")
-        parser.add_argument("--show_all","-sa",action="store_true",help="Shows all the news channel codes")
-        parser.add_argument("--news","-n",type=str,help="Shows news")
-    
-    
-        args=parser.parse_args()
-    
-        
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--show_all", "-sa", action="store_true",
+                            help="Shows all available news channel codes.")
+        parser.add_argument("--categories", "-c", action="store_true",
+                            help="Shows all available news categories.")
+        parser.add_argument("--show", "-s", action="store",
+                            help="Shows all news channel codes for a specified category.")
+        parser.add_argument("--news", "-n", type=str, help="Shows news articles "
+                            "for a specified news channel code.")
+        args = parser.parse_args()
+
         if args.show_all:
             show_sources_all()
+        elif args.categories:
+            show_categories()
         elif args.show:
             show_sources_category(args.show)
         elif args.news:
-             flag=0
-             l=args.news
-             if l in news_code:
-                 flag=1  
-             if flag==0:
-                 print("Enter valid newscode")
-                 sys.exit(1)
-             show_news(args.news,BASE_URL)   
-    
+            if args.news in news_codes:
+                show_news(args.news, BASE_URL)
+            else:
+                print("Invalid news code.")
+                sys.exit(1)
+
+
 def main():
+    """Test network connection, then parse arguments"""
     test_network_connection()
     parser()
-    
+
+
 if __name__ == "__main__":
-    main()    
+    main()
